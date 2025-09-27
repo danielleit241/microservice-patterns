b7e46395137d861d7c82e1e50ff7e950499fafd4
danielleit241 <danielleee241@gmail.com>
2025-09-27 15:04:55 +0700

    Add project files.

---
+++ Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.Dtos;
+
+namespace Play.Catalog.Service.Controllers
+{
+
+    [ApiController]
+    [Route("api/items")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Potion", "Restores 50 HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures 10 HP", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Sword", "Deals 50 damage", 20, DateTimeOffset.UtcNow)
+        };
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ItemDto>> Get()
+        {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetById(Guid id)
+        {
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Desccription, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            if (index < 0)
+            {
+                return NotFound();
+            }
+            var existingItem = items[index];
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Desccription = updateItemDto.Desccription,
+                Price = updateItemDto.Price
+            };
+            items[index] = updatedItem;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            if (index < 0)
+            {
+                return NotFound();
+            }
+            items.RemoveAt(index);
+            return NoContent();
+        }
+    }
+}
---
+++ Play.Catalog.Service/Dtos/ItemDtos.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Play.Catalog.Service.Dtos
+{
+    public record ItemDto(Guid Id, string Name, string Desccription, decimal Price, DateTimeOffset CreatedDate);
+
+    public record CreateItemDto([Required] string Name, string Desccription, [Range(0, 1000)] decimal Price);
+
+    public record UpdateItemDto([Required] string Name, string Desccription, [Range(0, 1000)] decimal Price);
+}
